7e40bf23c4a54ca59a6f602156a010434f2ae2a8
MahdiRamsar <rahimian.mohammadmahdi@gmail.com>
2026-07-09 08:42:12 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  build: {
-    outDir: 'dist',
-    ssr: false,
+  tanstackStart: {
+    server: {
+      entry: "server",
+    },
+  },
+
+  vite: {
+    base: "/",
+
+    build: {
+      outDir: "dist",
+      sourcemap: false,
+      emptyOutDir: true,
+    },
   },
 });
